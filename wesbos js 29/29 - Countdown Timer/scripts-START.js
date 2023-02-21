412d33ee675ea96154723892246fc3702cd23147
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-02-21 05:48:53 +0100

    added js

--- wesbos js 29/29 - Countdown Timer/scripts-START.js
+++ wesbos js 29/29 - Countdown Timer/scripts-START.js
@@ -4,7 +4,9 @@ function timer(seconds) {
     setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         // check if we should stop it
-
+        if(secondsLeft <= 0) {
+            return;
+        }
         // display it
         console.log(secondsLeft); 
     }, 1000);
