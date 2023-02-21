f5c58ed9c5ee7792b5333de235901b9f4d8efbc2
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-02-21 05:50:14 +0100

    added js

--- wesbos js 29/29 - Countdown Timer/scripts-START.js
+++ wesbos js 29/29 - Countdown Timer/scripts-START.js
@@ -1,7 +1,10 @@
+let countdown;
+
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
-    setInterval(() => {
+
+    countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         // check if we should stop it
         if(secondsLeft <= 0) {
