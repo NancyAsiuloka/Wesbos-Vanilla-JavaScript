d83157eb4123754a277f3b9d9f28930a8ce5880a
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-02-22 06:37:38 +0100

    added js

--- wesbos js 29/29 - Countdown Timer/scripts-START.js
+++ wesbos js 29/29 - Countdown Timer/scripts-START.js
@@ -1,6 +1,7 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 
 function timer(seconds) {
@@ -34,6 +35,14 @@ function displayTimeLeft(seconds) {
 function displayEndTime(timestamp) {
     const end = new Date(timestamp); 
     const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
     const minutes = end.getMinutes();
-    endTime.textContent = `Be Back At ${hour}:${minutes}`;
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    console.log(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
